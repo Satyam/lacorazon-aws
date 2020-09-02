5153d89b2f82f3d0aba72209b0159129792bf72a
Satyam <satyam@satyam.com.ar>
2020-09-02 19:26:01 +0200

    Used addCase and AddMatcher for reducers. addMatcher greately simplifies common operations

--- src/Store/distribuidores/reducer.ts
+++ src/Store/distribuidores/reducer.ts
@@ -1,4 +1,4 @@
-import { createReducer, SerializedError } from '@reduxjs/toolkit';
+import { createReducer, AnyAction, PayloadAction } from '@reduxjs/toolkit';
 import { IDLE, LOADING, LOADED, ThunkActionState } from 'Store/constants';
 import adapter from './adapter';
 
@@ -9,53 +9,40 @@ import {
   deleteDistribuidor,
 } from './actions';
 
-const setLoading = (state: ThunkActionState) => {
-  state.status = LOADING;
-  state.error = undefined;
-};
-
-const setError = (
-  state: ThunkActionState,
-  action: { error: SerializedError }
-) => {
-  state.status = IDLE;
-  state.error = action.error;
-};
-
 const reducer = createReducer(
   adapter.getInitialState<ThunkActionState>({
     status: IDLE,
   }),
-  {
-    [loadDistribuidores.pending.type]: setLoading,
-    [loadDistribuidores.rejected.type]: setError,
-    [loadDistribuidores.fulfilled.type]: (state, action) => {
-      state.status = LOADED;
-      adapter.setAll(state, action);
-    },
-    // createDistribuidor
-    [createDistribuidor.pending.type]: setLoading,
-    [createDistribuidor.rejected.type]: setError,
-    [createDistribuidor.fulfilled.type]: (state, action) => {
-      state.status = LOADED;
-      adapter.addOne(state, action);
-    },
-    // updateDistribuidor
-    [updateDistribuidor.pending.type]: setLoading,
-    [updateDistribuidor.rejected.type]: setError,
-    [updateDistribuidor.fulfilled.type]: (state, action) => {
-      state.status = LOADED;
-      const { idDistribuidor, ...changes } = action.payload;
-      adapter.updateOne(state, { id: idDistribuidor, changes });
-    },
-    // deleteDistribuidor
-    [deleteDistribuidor.pending.type]: setLoading,
-    [deleteDistribuidor.rejected.type]: setError,
-    [deleteDistribuidor.fulfilled.type]: (state, action) => {
-      state.status = LOADED;
-      adapter.removeOne(state, action);
-    },
-  }
+  (builder) =>
+    builder
+      .addCase(loadDistribuidores.fulfilled.type, adapter.setAll)
+      .addCase(createDistribuidor.fulfilled.type, adapter.addOne)
+      .addCase(updateDistribuidor.fulfilled.type, adapter.upsertOne)
+      .addCase(deleteDistribuidor.fulfilled.type, adapter.removeOne)
+      .addMatcher(
+        (action: AnyAction): action is PayloadAction<any> =>
+          action.type.endsWith('/pending'),
+        (state) => {
+          state.status = LOADING;
+          state.error = undefined;
+        }
+      )
+      .addMatcher(
+        (action: AnyAction): action is PayloadAction<any> =>
+          action.type.endsWith('/rejected'),
+        (state, action: AnyAction) => {
+          state.status = IDLE;
+          state.error = action.error;
+        }
+      )
+      .addMatcher(
+        (action: AnyAction): action is PayloadAction<any> =>
+          action.type.endsWith('/fulfilled'),
+        (state) => {
+          state.status = LOADED;
+          state.error = undefined;
+        }
+      )
 );
 
 export default reducer;
