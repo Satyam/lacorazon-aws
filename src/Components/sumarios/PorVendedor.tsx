adaeefe45cf57aefd820119fef548b09d1bd79b8
Satyam <satyam@satyam.com.ar>
2020-11-09 19:30:57 +0100

    Simplified sumarios/porVendedor

--- src/Components/sumarios/PorVendedor.tsx
+++ src/Components/sumarios/PorVendedor.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Table, Alert } from 'reactstrap';
 import Page from 'Components/Page';
 import { Loading } from 'Components/Modals';
+import { useVendedores } from 'Components/vendedores/common';
 import { useSalidas } from 'Components/salidas/common';
 import { useVentas } from 'Components/ventas/common';
 import { useConsignas } from 'Components/consigna/common';
@@ -23,6 +24,7 @@ type SumarioPorVendedor = {
 };
 
 const SumarioVendedores: React.FC = () => {
+  const [vendedores, loadingVendedores, errorVendedores] = useVendedores();
   const [salidas, loadingSalidas, errorSalidas] = useSalidas();
   const [ventas, loadingVentas, errorVentas] = useVentas();
   const [consignas, loadingConsigna, errorConsigna] = useConsignas();
@@ -31,9 +33,11 @@ const SumarioVendedores: React.FC = () => {
     loadingComisionInterna,
     errorComisionInterna,
   ] = useConfig('comisionInterna');
+
   const { formatCurrency } = useIntl();
 
   if (
+    loadingVendedores ||
     loadingSalidas ||
     loadingVentas ||
     loadingConsigna ||
@@ -41,180 +45,168 @@ const SumarioVendedores: React.FC = () => {
   )
     return <Loading>Cargando datos</Loading>;
 
-  if (salidas) {
-    const comisionesPagadasPorVendedor = salidas
-      .filter((salida) => salida.idVendedor)
-      .reduce<Record<string, any>>(
-        (acumuladorComisionesPorVendedor, salida) => {
-          const idVendedor = salida?.idVendedor || '';
-          acumuladorComisionesPorVendedor[idVendedor] =
-            (acumuladorComisionesPorVendedor[idVendedor] || 0) + salida.importe;
-          return acumuladorComisionesPorVendedor;
-        },
-        {}
-      );
-    if (ventas) {
-      if (comisionInterna) {
-        const ventasVendedores: Record<ID, SumarioPorVendedor> = {};
+  if (typeof salidas === 'undefined')
+    return <Alert color="warning">Tabla de salidas está vacía</Alert>;
+  if (typeof consignas === 'undefined')
+    return <Alert color="warning">Tabla de consignas está vacía</Alert>;
+  if (typeof comisionInterna === 'undefined')
+    return <Alert color="warning">Tabla de config está vacía</Alert>;
+  if (typeof ventas === 'undefined')
+    return <Alert color="warning">Tabla de ventas está vacía</Alert>;
+  if (typeof vendedores === 'undefined')
+    return <Alert color="warning">Tabla de vendedores está vacía</Alert>;
 
-        ventas.forEach(({ idVendedor = '??', precioUnitario, cantidad }) => {
-          let acumVtasPorVendedor = ventasVendedores[idVendedor];
+  const ventasVendedores = vendedores.reduce<Record<ID, SumarioPorVendedor>>(
+    (vvs, v) => ({
+      ...vvs,
+      [v.idVendedor]: {
+        idVendedor: v.idVendedor,
+        vendido: 0,
+        regalado: 0,
+        total: 0,
+        precio: 0,
+        comisionVD: 0,
+        comisionEC: 0,
+        comisionPagada: 0,
+        promedio: 0,
+      },
+    }),
+    {}
+  );
 
-          if (!acumVtasPorVendedor) {
-            acumVtasPorVendedor = {
-              idVendedor,
-              vendido: 0,
-              regalado: 0,
-              total: 0,
-              precio: 0,
-              comisionVD: 0,
-              comisionEC: 0,
-              comisionPagada: comisionesPagadasPorVendedor[idVendedor] || 0,
-              promedio: 0,
-            };
-            ventasVendedores[idVendedor] = acumVtasPorVendedor;
-          }
+  salidas.forEach((salida) => {
+    if (salida.idVendedor) {
+      ventasVendedores[salida.idVendedor].comisionPagada += salida.importe;
+    }
+  });
 
-          acumVtasPorVendedor.total += cantidad;
-          if (precioUnitario) {
-            acumVtasPorVendedor.vendido += cantidad;
-            acumVtasPorVendedor.precio += cantidad * precioUnitario;
-            acumVtasPorVendedor.comisionVD +=
-              cantidad * precioUnitario * comisionInterna;
-          } else {
-            acumVtasPorVendedor.regalado += cantidad;
-          }
-        });
+  ventas.forEach(({ idVendedor = '??', precioUnitario, cantidad }) => {
+    const acumVtasPorVendedor = ventasVendedores[idVendedor];
 
-        for (const v in ventasVendedores) {
-          var row = ventasVendedores[v];
-          if (row.vendido) {
-            row.promedio = row.precio / row.vendido;
-          }
-        }
-        if (consignas) {
-          consignas
-            .filter((consigna) => consigna.facturado > 0 && consigna.idVendedor)
-            .forEach(({ idVendedor, facturado }) => {
-              ventasVendedores[idVendedor].comisionEC +=
-                comisionInterna * facturado;
-            });
+    acumVtasPorVendedor.total += cantidad;
+    if (precioUnitario) {
+      acumVtasPorVendedor.vendido += cantidad;
+      acumVtasPorVendedor.precio += cantidad * precioUnitario;
+      acumVtasPorVendedor.comisionVD +=
+        cantidad * precioUnitario * comisionInterna;
+    } else {
+      acumVtasPorVendedor.regalado += cantidad;
+    }
+  });
 
-          const totales = Object.values(ventasVendedores).reduce<
-            SumarioPorVendedor
-          >(
-            (totales, sumario) => ({
-              ...totales,
-              vendido: totales.vendido + sumario.vendido,
-              regalado: totales.regalado + sumario.regalado,
-              total: totales.total + sumario.total,
-              precio: totales.precio + sumario.precio,
-            }),
-            {
-              idVendedor: '',
-              vendido: 0,
-              regalado: 0,
-              total: 0,
-              precio: 0,
-              comisionVD: 0,
-              comisionEC: 0,
-              comisionPagada: 0,
-              promedio: 0,
-            }
-          );
-          const rowSumario = (sumario: SumarioPorVendedor) => {
-            const idVendedor = sumario.idVendedor;
-            return (
-              <tr key={idVendedor}>
-                <td align="right">
-                  <ShowVendedor idVendedor={idVendedor} />
-                </td>
-                <td align="right">{sumario.vendido}</td>
-                <td align="right">{sumario.regalado}</td>
-                <td align="right">{sumario.total}</td>
-                <td align="right">{formatCurrency(sumario.precio)}</td>
-                <td align="right">{formatCurrency(sumario.promedio)}</td>
-                <td align="right">
-                  {idVendedor === 'rora'
-                    ? ''
-                    : formatCurrency(sumario.comisionVD)}
-                </td>
-                <td align="right">
-                  {idVendedor === 'rora'
-                    ? ''
-                    : formatCurrency(sumario.comisionEC)}
-                </td>
-                <td align="right">
-                  {idVendedor === 'rora'
-                    ? ''
-                    : formatCurrency(-sumario.comisionPagada)}
-                </td>
-                <td align="right">
-                  {idVendedor === 'rora'
-                    ? ''
-                    : formatCurrency(
-                        sumario.comisionVD +
-                          sumario.comisionEC -
-                          sumario.comisionPagada
-                      )}
-                </td>
-              </tr>
-            );
-          };
+  for (const v in ventasVendedores) {
+    var row = ventasVendedores[v];
+    if (row.vendido) {
+      row.promedio = row.precio / row.vendido;
+    }
+  }
+  consignas
+    .filter((consigna) => consigna.facturado > 0 && consigna.idVendedor)
+    .forEach(({ idVendedor, facturado }) => {
+      ventasVendedores[idVendedor].comisionEC += comisionInterna * facturado;
+    });
 
-          return (
-            <Page
-              wide
-              title="Sumario Vendedores"
-              heading="Sumario Vendedores"
-              error={
-                errorSalidas?.message ||
-                errorVentas?.message ||
-                errorConsigna?.message ||
-                errorComisionInterna?.message
-              }
-            >
-              <Table striped hover size="sm" responsive bordered>
-                <thead>
-                  <tr>
-                    <th rowSpan={2}>Vendedor</th>
-                    <th colSpan={3}>Cantidades</th>
-                    <th colSpan={2}>Cobrado</th>
-                    <th colSpan={4}>Comisiones</th>
-                  </tr>
-                  <tr>
-                    <th>Vendidos</th>
-                    <th>Regalados</th>
-                    <th>Total</th>
-                    <th>Total</th>
-                    <th>Promedio por libro</th>
-                    <th>por Venta Directa</th>
-                    <th>por Distribuidores</th>
-                    <th>Pagada</th>
-                    <th>A Pagar</th>
-                  </tr>
-                </thead>
-                <tbody>{Object.values(ventasVendedores).map(rowSumario)}</tbody>
-                <tbody style={{ borderTop: 'silver double' }}>
-                  <tr>
-                    <th>Totales</th>
-                    <td align="right">{totales.vendido}</td>
-                    <td align="right">{totales.regalado}</td>
-                    <td align="right">{totales.total}</td>
-                    <td align="right">{formatCurrency(totales.precio)}</td>
-                    <td align="right">
-                      {formatCurrency(totales.precio / totales.vendido)}
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Page>
-          );
-        } else
-          return <Alert color="warning">Tabla de consignas está vacía</Alert>;
-      } else return <Alert color="warning">Tabla de config está vacía</Alert>;
-    } else return <Alert color="warning">Tabla de ventas está vacía</Alert>;
-  } else return <Alert color="warning">Tabla de salidas está vacía</Alert>;
+  const totales = Object.values(ventasVendedores).reduce<SumarioPorVendedor>(
+    (totales, sumario) => ({
+      ...totales,
+      vendido: totales.vendido + sumario.vendido,
+      regalado: totales.regalado + sumario.regalado,
+      total: totales.total + sumario.total,
+      precio: totales.precio + sumario.precio,
+    }),
+    {
+      idVendedor: '',
+      vendido: 0,
+      regalado: 0,
+      total: 0,
+      precio: 0,
+      comisionVD: 0,
+      comisionEC: 0,
+      comisionPagada: 0,
+      promedio: 0,
+    }
+  );
+  const rowSumario = (sumario: SumarioPorVendedor) => {
+    const idVendedor = sumario.idVendedor;
+    return (
+      <tr key={idVendedor}>
+        <td align="right">
+          <ShowVendedor idVendedor={idVendedor} />
+        </td>
+        <td align="right">{sumario.vendido}</td>
+        <td align="right">{sumario.regalado}</td>
+        <td align="right">{sumario.total}</td>
+        <td align="right">{formatCurrency(sumario.precio)}</td>
+        <td align="right">{formatCurrency(sumario.promedio)}</td>
+        <td align="right">
+          {idVendedor === 'rora' ? '' : formatCurrency(sumario.comisionVD)}
+        </td>
+        <td align="right">
+          {idVendedor === 'rora' ? '' : formatCurrency(sumario.comisionEC)}
+        </td>
+        <td align="right">
+          {idVendedor === 'rora' ? '' : formatCurrency(-sumario.comisionPagada)}
+        </td>
+        <td align="right">
+          {idVendedor === 'rora'
+            ? ''
+            : formatCurrency(
+                sumario.comisionVD + sumario.comisionEC - sumario.comisionPagada
+              )}
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <Page
+      wide
+      title="Sumario Vendedores"
+      heading="Sumario Vendedores"
+      error={
+        errorVendedores?.message ||
+        errorSalidas?.message ||
+        errorVentas?.message ||
+        errorConsigna?.message ||
+        errorComisionInterna?.message
+      }
+    >
+      <Table striped hover size="sm" responsive bordered>
+        <thead>
+          <tr>
+            <th rowSpan={2}>Vendedor</th>
+            <th colSpan={3}>Cantidades</th>
+            <th colSpan={2}>Cobrado</th>
+            <th colSpan={4}>Comisiones</th>
+          </tr>
+          <tr>
+            <th>Vendidos</th>
+            <th>Regalados</th>
+            <th>Total</th>
+            <th>Total</th>
+            <th>Promedio por libro</th>
+            <th>por Venta Directa</th>
+            <th>por Distribuidores</th>
+            <th>Pagada</th>
+            <th>A Pagar</th>
+          </tr>
+        </thead>
+        <tbody>{Object.values(ventasVendedores).map(rowSumario)}</tbody>
+        <tbody style={{ borderTop: 'silver double' }}>
+          <tr>
+            <th>Totales</th>
+            <td align="right">{totales.vendido}</td>
+            <td align="right">{totales.regalado}</td>
+            <td align="right">{totales.total}</td>
+            <td align="right">{formatCurrency(totales.precio)}</td>
+            <td align="right">
+              {formatCurrency(totales.precio / totales.vendido)}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Page>
+  );
 };
 
 export default SumarioVendedores;
