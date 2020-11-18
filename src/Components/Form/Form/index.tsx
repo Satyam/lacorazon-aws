6cd4e98d4825e0afb015ee2d7a60e7d2fa7efd5e
Satyam <satyam@satyam.com.ar>
2020-11-18 16:34:57 +0100

    Removal of source files no longer in use

--- src/Components/Form/Form/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Form } from 'reactstrap';
-
-const MyForm: React.FC = ({ children, ...rest }) => {
-  return <Form {...rest}>{children}</Form>;
-};
-
-export default MyForm;
--- src/Components/Form/index.ts
+++ src/Components/Form/index.ts
@@ -4,4 +4,3 @@ export { default as CheckboxField } from './CheckboxFIeld';
 export { default as DropdownField } from './DropdownField';
 export { default as SubmitButton } from './SubmitButton';
 export { LabeledCheckbox, LabeledText } from './LabeledField';
-export { default as Form } from './Form';
