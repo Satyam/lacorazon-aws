4e5821786ae204e7388b1b4d28432a48aa54ec7e
Satyam <satyam@satyam.com.ar>
2020-11-25 19:25:17 +0100

    Fixes #13

--- migrate/upload.js
+++ migrate/upload.js
@@ -4,44 +4,72 @@ const serviceAccount = require('./lacorazon-d66fd-firebase-adminsdk-thy14-92e97d
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://lacorazon-d66fd.firebaseio.com'
+  databaseURL: 'https://lacorazon-d66fd.firebaseio.com',
 });
 
 const db = admin.database();
 
 const data = require('./La Corazon.json');
 
+function filterEmpty(obj) {
+  return Object.keys(obj).reduce((out, k) => {
+    const val = obj[k];
+    return typeof val === 'undefined' || val === null
+      ? out
+      : {
+          ...out,
+          [k]: val,
+        };
+  }, {});
+}
 function addVendedores() {
   console.log('vendedores');
   return db.ref('vendedores').set({
     ro: {
       nombre: 'Roxana Cabut',
-      email: 'RoxanaCabut@gmail.com'
+      email: 'RoxanaCabut@gmail.com',
     },
-    ra:{
+    ra: {
       nombre: 'Raed El Younsi',
-      email: 'reyezuelo@gmail.com'
+      email: 'reyezuelo@gmail.com',
     },
-    rora:{
+    rora: {
       nombre: 'Roxana & Raed',
-      email: 'reyezuelo@gmail.com;RoxanaCabut@gmail.com'
-    }
-  })
+      email: 'reyezuelo@gmail.com;RoxanaCabut@gmail.com',
+    },
+  });
 }
 
+function addUsers() {
+  console.log('users');
+  return db.ref('users').set({
+    118059719715363805543: {
+      role: 'admin',
+    },
+    // 'roxanacabut@gmail.com': {
+    //   role: 'operador'
+    // },
+    // 'reyezuelo@gmail.com': {
+    //   role: 'operador'
+    // }
+  });
+}
+
+const PVP = 12;
+const comisionEstandar = 0.35;
 function addConfig() {
   console.log('config');
   return db.ref('config').set({
-    PVP:	12,
-    comisionEstandar:0.35,
-    IVALibros:0.04,
-    comisionInterna:0.25,
-    IVAs: [0,0.04, 0.10, 0.21]
-})
+    PVP: 12,
+    comisionEstandar: 0.35,
+    IVALibros: 0.04,
+    comisionInterna: 0.25,
+    IVAs: [0, 0.04, 0.1, 0.21],
+  });
 }
 
 function addDistribuidores() {
-  console.log('distribuidores')
+  console.log('distribuidores');
   const campos = [
     'nombre',
     'localidad',
@@ -49,18 +77,25 @@ function addDistribuidores() {
     'telefono',
     'email',
     'direccion',
-    'nif'
+    'nif',
   ];
   return db.ref('distribuidores').set(
-    data.puntosDeVenta.reduce((distribuidores, {codigo, ...distribuidor}) => ({
-      ...distribuidores,
-      [codigo.toLowerCase()]: campos.reduce((acc, campo) => distribuidor[campo] 
-        ? ({
-          ...acc,
-          [campo]:distribuidor[campo]
-        })
-        : acc, {})
-    }), {})
+    data.puntosDeVenta.reduce(
+      (distribuidores, { codigo, ...distribuidor }) => ({
+        ...distribuidores,
+        [codigo.toLowerCase()]: campos.reduce(
+          (acc, campo) =>
+            distribuidor[campo]
+              ? {
+                  ...acc,
+                  [campo]: distribuidor[campo],
+                }
+              : acc,
+          {}
+        ),
+      }),
+      {}
+    )
   );
 }
 
@@ -73,79 +108,163 @@ function byFecha(a, b) {
 }
 
 function addVentaDirecta() {
-  console.log('venta directa')
+  console.log('venta directa');
   const ventas = db.ref('ventas');
   return Promise.all(
-    data.ventaDirecta.sort(byFecha).map(({vendedor, ctaRaed, ...venta}) =>
+    data.ventaDirecta.sort(byFecha).map(({ vendedor, ctaRaed, ...venta }) =>
       ventas.push({
         ...venta,
         idVendedor: vendedor ? vendedor.toLowerCase() : null,
-        cuenta: ctaRaed ? 'ctaRaed' : 'efvoRoxy'
+        cuenta: ctaRaed ? 'ctaRaed' : 'efvoRoxy',
       })
     )
   );
 }
 
 function addEnConsigna() {
-  console.log('en consigna')
+  console.log('en consigna');
   const consigna = db.ref('consigna');
+  const facturas = db.ref('facturas');
+  const porcentajes = {};
   return Promise.all(
-    data.enConsigna.sort(byFecha).map(({ codigo, vendedor, ctaRaed, ...venta }) =>
-      consigna.push({
-        ...venta,
-        idVendedor: vendedor ? vendedor.toLowerCase() : null,
-        idDistribuidor: codigo.toLowerCase(),
-        cuenta: ctaRaed ? 'ctaRaed' : 'efvoRoxy'
-
-      })
-    )
+    data.enConsigna
+      .sort(byFecha)
+      .map(
+        ({
+          fecha,
+          codigo,
+          vendedor,
+          ctaRaed,
+          entregados,
+          vendidos,
+          devueltos,
+          facturado,
+          nroFactura,
+          porcentaje,
+          cobrado,
+          comentarios,
+        }) => {
+          const idDistribuidor = codigo.toLowerCase();
+          const concepto = comentarios;
+          if (porcentaje) {
+            porcentajes[idDistribuidor] = porcentaje;
+          }
+          if (entregados) {
+            return consigna.push(
+              filterEmpty({
+                idDistribuidor,
+                fecha,
+                concepto,
+                movimiento: 'entregados',
+                cantidad: entregados,
+              })
+            );
+          }
+          if (vendidos) {
+            return consigna.push(
+              filterEmpty({
+                idDistribuidor,
+                fecha,
+                concepto,
+                movimiento: 'vendidos',
+                cantidad: vendidos,
+              })
+            );
+          }
+          if (devueltos) {
+            return consigna.push(
+              filterEmpty({
+                idDistribuidor,
+                fecha,
+                concepto,
+                movimiento: 'devueltos',
+                cantidad: devueltos,
+              })
+            );
+          }
+          if (facturado) {
+            return consigna.push(
+              filterEmpty({
+                idDistribuidor,
+                fecha,
+                concepto,
+                movimiento: 'facturados',
+                cantidad: Math.round(
+                  facturado /
+                    (1 - (porcentajes[idDistribuidor] || comisionEstandar)) /
+                    PVP
+                ),
+              })
+            );
+          }
+          return facturas.push(
+            filterEmpty({
+              idDistribuidor,
+              fecha,
+              concepto,
+              idVendedor: vendedor ? vendedor.toLowerCase() : null,
+              porcentaje: porcentajes[idDistribuidor] || comisionEstandar,
+              nroFactura,
+              cobrado,
+              cuenta: ctaRaed ? 'ctaRaed' : 'efvoRoxy',
+            })
+          );
+        }
+      )
   );
 }
 
 function addSalidas() {
-  console.log('salidas')
+  console.log('salidas');
   const gastos = db.ref('gastos');
   const reintegros = db.ref('reintegros');
   const comisiones = db.ref('comisiones');
-  const pagosIva = db.ref('pagosIva')
+  const pagosIva = db.ref('pagosIva');
   return Promise.all(
-    data.salidas.sort(byFecha).map(({comision, ctaRaed, reintegro, pagoiva, iva, ...salida}) =>{
-      const cuenta = ctaRaed ? 'ctaRaed' : 'efvoRoxy';
+    data.salidas
+      .sort(byFecha)
+      .map(({ comision, ctaRaed, reintegro, pagoiva, iva, ...salida }) => {
+        const cuenta = ctaRaed ? 'ctaRaed' : 'efvoRoxy';
 
-      if (reintegro) return reintegros.push({
-        ...salida,
-        cuenta
-      })
-      if (pagoiva) return pagosIva.push({
-        ...salida,
-        cuenta
-      })
-      if (comision) return comisiones.push({
-        ...salida,
-        idVendedor: comision ? comision.toLowerCase():  null
-      })
-      return gastos.push(
-        {
+        if (reintegro)
+          return reintegros.push({
+            ...salida,
+            cuenta,
+          });
+        if (pagoiva)
+          return pagosIva.push({
+            ...salida,
+            cuenta,
+          });
+        if (comision)
+          return comisiones.push({
+            ...salida,
+            idVendedor: comision ? comision.toLowerCase() : null,
+          });
+        return gastos.push({
           ...salida,
           iva: iva || 0,
-          cuenta
-        }
-      )}
-  ));
+          cuenta,
+        });
+      })
+  );
 }
 
-db.ref().set({
-  config: null,
-  vendedores: null,
-  distribuidores: null,
-  consigna: null,
-  gastos: null,
-  reintegros: null,
-  comisiones: null,
-  pagosIva: null
-})
+db.ref()
+  .set({
+    config: null,
+    vendedores: null,
+    distribuidores: null,
+    consigna: null,
+    facturas: null,
+    gastos: null,
+    reintegros: null,
+    comisiones: null,
+    pagosIva: null,
+  })
   .then(addConfig)
   .then(addVendedores)
+  .then(addUsers)
   .then(addDistribuidores)
   .then(addVentaDirecta)
   .then(addEnConsigna)
