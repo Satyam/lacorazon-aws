14be1f0f661cac69124e9cfbfaad2e9b0f4eb465
Satyam <satyam@satyam.com.ar>
2020-11-28 19:40:19 +0100

    Added loading and loadingMsg to Components/Page

--- src/Components/Page/index.tsx
+++ src/Components/Page/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
 import { ErrorAlert, ErrorAlertError } from 'Components/ErrorAlert';
+import Loading from 'Components/Modals/Loading';
 import styles from './styles.module.css';
 
 const Page: React.FC<{
@@ -10,23 +11,37 @@ const Page: React.FC<{
   heading: string;
   action?: React.ReactNode;
   error?: ErrorAlertError;
-}> = ({ wide, children, title, heading, action, error }) => {
+  loading?: boolean;
+  loadingMsg?: React.ReactNode;
+}> = ({
+  wide,
+  children,
+  title,
+  heading,
+  action,
+  error,
+  loading,
+  loadingMsg,
+}) => {
   useEffect(() => {
     if (title) document.title = `La Corazón - ${title}`;
   }, [title]);
   return (
-    <Container fluid>
-      <Row>
-        <Col sm="12" md={{ size: wide ? 12 : 8, offset: wide ? 0 : 2 }}>
-          <div className={classnames('clearfix', styles.spacing)}>
-            <h1 className={styles.heading}>{heading}</h1>
-            <div className={styles.action}>{action}</div>
-          </div>
-          <ErrorAlert error={error}>{heading}</ErrorAlert>
-          {children}
-        </Col>
-      </Row>
-    </Container>
+    <>
+      {loading && <Loading centered>{loadingMsg}</Loading>}
+      <Container fluid>
+        <Row>
+          <Col sm="12" md={{ size: wide ? 12 : 8, offset: wide ? 0 : 2 }}>
+            <div className={classnames('clearfix', styles.spacing)}>
+              <h1 className={styles.heading}>{heading}</h1>
+              <div className={styles.action}>{action}</div>
+            </div>
+            <ErrorAlert error={error}>{heading}</ErrorAlert>
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
